f8fbd71add9fea8e8a032c535c4f1efae311bc32
Lily Thomas <lilylynettethomas@gmail.com>
2022-02-02 22:40:11 -0800

    Added info for department and role table

---
+++ develop/db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department(dpt_name)
+VALUES("Engineering"),
+      ("Finance"),
+      ("Legal"),
+      ("Sales");
+
+INSERT INTO role(title, salary, department_id)
+VALUES("Sales Lead", 100.0, 4),
+      ("Salesperson", 80.0, 4),
+      ("Lead Engineer", 150.0, 1),
+      ("Software Engineer", 120.0, 1),
+      ("Account Manager", 160.0, 2),
+      ("Accountant", 125.0, 2),
+      ("Legal Team Lead", 250.0, 3),
+      ("Lawyer", 190.0, 3);
